ce78569e3cbfce7891dae4b42206823e6ddfb329
dmitry <arbuzov.mail@gmail.com>
2019-01-23 01:14:00 +0300

    add version 1.0.0

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'version' => '1.0.0',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'defaultRoute' => 'category/index',
